346a378f218aff065c49de5a6a973b62f4391af6
do tung <dotung010597@gmail.com>
2024-05-19 10:47:45 +0700

    thêm animation

--- fashionhub/lib/view/login_screen.dart
+++ fashionhub/lib/view/login_screen.dart
@@ -1,12 +1,19 @@
+import 'package:fashionhub/animation/animation.dart';
 import 'package:fashionhub/service/authentication_service.dart';
 import 'package:fashionhub/view/signup_screen.dart';
 import 'package:fashionhub/viewmodel/login_viewmodel.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+
 import 'home_screen.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final LoginViewModel _loginViewModel =
@@ -27,156 +34,160 @@ class LoginScreen extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Image.asset(
-                      'assets/images/logo.png',
-                      width: 200,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    TextFormField(
-                      controller: emailController,
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        prefixIcon: Icon(Icons.email_outlined),
-                        labelText: 'Email',
-                        hintText: 'Enter your Email',
-                        hintStyle: TextStyle(
-                          color: Colors.black26,
-                        ),
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.white, // Default border color
-                          ),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.white, // Default border color
-                          ),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
+                    FadeAnimation(
+                      0.1,
+                      Image.asset(
+                        'assets/images/logo.png',
+                        width: 200,
                       ),
                     ),
-                    const SizedBox(
-                      height: 25.0,
-                    ),
-                    TextFormField(
-                      controller: passwordController,
-                      obscureText: true,
-                      obscuringCharacter: '*',
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        prefixIcon: Icon(Icons.password_outlined),
-                        labelText: 'Password',
-                        hintText: 'Enter your Password',
-                        hintStyle: TextStyle(
-                          color: Colors.black26,
-                        ),
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.black12, // Default border color
-                          ),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.black12, // Default border color
-                          ),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 25.0,
-                    ),
-                    SizedBox(
-                      width: 150,
-                      child: ElevatedButton(
-                        onPressed: () async {
-                          String email = emailController.text.trim();
-                          String password = passwordController.text.trim();
-                          User? user = await _loginViewModel
-                              .signInWithEmailAndPassword(email, password);
-                          if (user != null) {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => HomeScreen()),
-                            );
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(content: Text('Login failed')));
-                          }
-                        },
-                        child: Text('Login'),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 25.0,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          child: Divider(
-                            thickness: 0.7,
-                            color: Colors.black.withOpacity(0.5),
-                          ),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(
-                            vertical: 0,
-                            horizontal: 10,
-                          ),
-                          child: Text(
-                            'OR',
-                            style: TextStyle(
-                              color: Colors.black45,
-                              fontSize: 17,
+                    SizedBox(height: 20),
+                    FadeAnimation(
+                      0.2,
+                      TextFormField(
+                        controller: emailController,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          prefixIcon: Icon(Icons.email_outlined),
+                          labelText: 'Email',
+                          hintText: 'Enter your Email',
+                          hintStyle: TextStyle(color: Colors.black26),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.white, // Default border color
                             ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.white, // Default border color
+                            ),
+                            borderRadius: BorderRadius.circular(10),
                           ),
                         ),
-                        Expanded(
-                          child: Divider(
-                            thickness: 0.7,
-                            color: Colors.black.withOpacity(0.5),
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
-                    const SizedBox(
-                      height: 30.0,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          'Bạn chưa có tài khoản? ',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 17,
+                    const SizedBox(height: 25.0),
+                    FadeAnimation(
+                      0.3,
+                      TextFormField(
+                        controller: passwordController,
+                        obscureText: true,
+                        obscuringCharacter: '*',
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          prefixIcon: Icon(Icons.password_outlined),
+                          labelText: 'Password',
+                          hintText: 'Enter your Password',
+                          hintStyle: TextStyle(color: Colors.black26),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black12, // Default border color
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black12, // Default border color
+                            ),
+                            borderRadius: BorderRadius.circular(10),
                           ),
                         ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => SignUpScreen()),
-                            );
+                      ),
+                    ),
+                    const SizedBox(height: 25.0),
+                    FadeAnimation(
+                      0.4,
+                      SizedBox(
+                        width: 150,
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            String email = emailController.text.trim();
+                            String password = passwordController.text.trim();
+                            User? user = await _loginViewModel
+                                .signInWithEmailAndPassword(email, password);
+                            if (user != null) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => HomeScreen()),
+                              );
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(content: Text('Login failed')));
+                            }
                           },
-                          child: const Text(
-                            'Đăng ký',
-                            style: TextStyle(
-                              fontSize: 17,
-                              fontWeight: FontWeight.bold,
+                          child: Text('Login'),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 25.0),
+                    FadeAnimation(
+                      0.5,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: Divider(
+                              thickness: 0.7,
+                              color: Colors.black.withOpacity(0.5),
                             ),
                           ),
-                        ),
-                      ],
+                          const Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 0,
+                              horizontal: 10,
+                            ),
+                            child: Text(
+                              'OR',
+                              style: TextStyle(
+                                color: Colors.black45,
+                                fontSize: 17,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Divider(
+                              thickness: 0.7,
+                              color: Colors.black.withOpacity(0.5),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 30.0),
+                    FadeAnimation(
+                      0.6,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Bạn chưa có tài khoản? ',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 17,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SignUpScreen()),
+                              );
+                            },
+                            child: const Text(
+                              'Đăng ký',
+                              style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
